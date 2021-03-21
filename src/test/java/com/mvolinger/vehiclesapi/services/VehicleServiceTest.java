306f5ea7c58c020edf7abd508a034ffab2998547
Monise Volinger <monise.correa@projuris.com.br>
2021-03-21 13:24:21 -0300

    Test adjustments.

--- src/test/java/com/mvolinger/vehiclesapi/services/VehicleServiceTest.java
+++ src/test/java/com/mvolinger/vehiclesapi/services/VehicleServiceTest.java
@@ -1,16 +1,20 @@
 package com.mvolinger.vehiclesapi.services;
 
 import com.mvolinger.vehiclesapi.controllers.AbstractRestController;
+import com.mvolinger.vehiclesapi.dto.FuelConsumptionDTO;
 import com.mvolinger.vehiclesapi.dto.VehicleDTO;
 import com.mvolinger.vehiclesapi.entities.Vehicle;
 import com.mvolinger.vehiclesapi.repositories.VehicleRepository;
 import com.mvolinger.vehiclesapi.services.impl.VehicleServiceImpl;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import static org.mockito.Mockito.*;
 
@@ -20,8 +24,11 @@ public class VehicleServiceTest extends AbstractRestController {
   private static final String VEHICLE_NAME = String.valueOf(new Random().nextInt());
   private static final String VEHICLE_BRAND = String.valueOf(new Random().nextInt());
   private static final String VEHICLE_MODEL = String.valueOf(new Random().nextInt());
-  private static final Double CITY_FUEL_CONSUMPTION = 0.415540283284770;
-  private static final Double HIGHWAY_FUEL_CONSUMPTION = 0.5489156548180;
+  private static final Double CITY_FUEL_CONSUMPTION = 1.0;
+  private static final Double HIGHWAY_FUEL_CONSUMPTION = 1.0;
+  private static final Double FUEL_PRICE = 1.0;
+  private static final Double CITY_KM = 1.0;
+  private static final Double HIGHWAY_KM = 1.0;
 
   @Mock
   private VehicleRepository vehicleRepository;
@@ -58,4 +65,32 @@ public class VehicleServiceTest extends AbstractRestController {
 
     verify(vehicleRepository, times(1)).save(refEq(vehicle));
   }
+
+  @Test
+  public void shouldGetFuelConsumptionSuccessfully() {
+      Vehicle vehicle = Vehicle.builder()
+              .name(VEHICLE_NAME)
+              .brand(VEHICLE_BRAND)
+              .model(VEHICLE_MODEL)
+              .cityFuelConsumption(CITY_FUEL_CONSUMPTION)
+              .highwayFuelConsumption(HIGHWAY_FUEL_CONSUMPTION)
+              .build();
+
+      List<Vehicle> allVehicles = new ArrayList<>();
+      allVehicles.add(vehicle);
+
+      when(vehicleRepository.findAll()).thenReturn(allVehicles);
+
+      List<FuelConsumptionDTO> fuelConsumptionDTOS = vehicleService.getFuelConsumption(FUEL_PRICE, CITY_KM, HIGHWAY_KM);
+
+      Assertions.assertEquals(1, fuelConsumptionDTOS.size());
+      Assertions.assertEquals(vehicle.getName(), fuelConsumptionDTOS.get(0).getName());
+      Assertions.assertEquals(vehicle.getBrand(), fuelConsumptionDTOS.get(0).getBrand());
+      Assertions.assertEquals(vehicle.getModel(), fuelConsumptionDTOS.get(0).getModel());
+      Assertions.assertEquals(vehicle.getManufactureDate(), fuelConsumptionDTOS.get(0).getManufactureDate());
+      Assertions.assertEquals(2.0, fuelConsumptionDTOS.get(0).getConsumedFuel());
+      Assertions.assertEquals(2.0, fuelConsumptionDTOS.get(0).getAmountSpent());
+
+  }
+
 }
